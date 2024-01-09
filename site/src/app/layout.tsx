8fa401b0ea78ecad280f60d9c7d368514660be27
Martin Miglio <marmig0404@gmail.com>
2024-01-09 14:32:28 -0500

    initial site setup

---
+++ site/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CS471 Project",
+  description:
+    "A project to demonstrate the principles of Software Engineering",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
