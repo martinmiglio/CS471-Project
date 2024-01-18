3d918cda4991936560af0306ba9a6e9dce216880
Martin Miglio <marmig0404@gmail.com>
2024-01-18 18:19:07 -0500

    add basic landing page

---
+++ site/src/app/landing/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>CS471 Project</h1>
+      <p>A project to demonstrate the principles of Software Engineering</p>
+    </div>
+  );
+}
