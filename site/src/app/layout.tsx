d303ea8c2b70b80a51c2d9902ae0cab6c965b9f6
Martin Miglio <marmig0404@gmail.com>
2024-01-20 20:49:33 -0500

    add analytics script to site (#6)

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+import { z } from "zod";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const schema = z.object({
+  ANALYTICS_ID: z.string(),
+  ANALYTICS_SCRIPT_URL: z.string(),
+  ANALYTICS_DOMAINS: z.string().optional(),
+});
+const env = schema.parse(process.env);
 
 export const metadata: Metadata = {
   title: "CS471 Project",
@@ -17,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={env.ANALYTICS_SCRIPT_URL}
+          data-website-id={env.ANALYTICS_ID}
+          data-domains={env.ANALYTICS_DOMAINS}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
