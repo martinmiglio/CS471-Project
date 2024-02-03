1406ccd288734e28ba077a480aa9c5ee96950a51
Martin Miglio <marmig0404@gmail.com>
2024-02-02 20:36:10 -0500

    fix landing styling

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl ">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
           Biddr. Bid More. Be Happy.
         </h1>
         <p className="mb-8 text-lg font-normal text-primary sm:px-16 lg:px-48 lg:text-xl">
@@ -23,14 +23,17 @@ export default function Page() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
           </Button>
-          <Button className="inline-flex items-center justify-center px-5 py-3 sm:ms-4">
+          <Button
+            className="inline-flex items-center justify-center px-5 py-3 sm:ms-4"
+            variant="outline"
+          >
             Learn more
           </Button>
         </div>
