5f2b5498ed698e51c666c16c3ba07903fcbc57f1
AaronDee13 <aarondehaven13@gmail.com>
2024-02-07 16:19:15 -0500

    Reoriented UN/PW Bubbles, Styled Login button, added Signup Button, and linked Landing Page login button to Login page

--- site/src/app/login/page.tsx
+++ site/src/app/login/page.tsx
@@ -16,29 +16,35 @@ export default function Page() {
   // TODO: Implement login page
   return (    
   <section className="bg-white dark:bg-gray-900">
-    <button>Login</button> 
+
 
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Biddr. Bid More. Be Happy.</h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Buy, Sell, Trade!</p>
-          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-              
             <div>
-              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Login</h1>
+              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome!</h1>
             </div>
-            
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <div className="mb-6">
-              <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+              <label htmlFor="default-input" className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Username</label>
               <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-            </div>
-                
-            <div className="mb-6">
-              <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-            </div>    
-                       
+            </div>          
           </div>
-      </div>  
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <div className="mb-6">
+                <label htmlFor="default-input" className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Password</label>
+                <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+            </div>  
+          </div>
+      </div>
+
+
+    <div  className="flex justify-center space-x-8">
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-md">Login</button>
+      <button className="bg-green-600 text-white px-4 py-2 rounded-md">Sign Up</button>
+    </div>
   </section>
+
+
   );
 }
--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Biddr. Bid More. Be Happy.</h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Buy, Sell, Trade!</p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                <a href="/login" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     Log In
                     <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
