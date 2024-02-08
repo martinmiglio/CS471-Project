afca97e7ce29cdccca3bc0e0c89658826d86e800
robi9661 <156124302+robi9661@users.noreply.github.com>
2024-02-08 16:26:00 -0500

    Add login page (#16)
    Co-authored-by: Martin Miglio <marmig0404@gmail.com>
    Co-authored-by: AaronDee13 <aarondehaven@ymail.com>
    Co-authored-by: AaronDee13 <aarondehaven13@gmail.com>

---
+++ site/src/components/Jumbotron.tsx
@@ -0,0 +1,12 @@
+export default function Jumbotron() {
+  return (
+    <>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        Biddr. Bid More. Be Happy.
+      </h1>
+      <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
+        Buy, Sell, Trade!
+      </p>
+    </>
+  );
+}
