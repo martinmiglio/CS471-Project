6c12f4f276b51a7cb8ed21d76a418cecd41af5bb
Martin Miglio <marmig0404@gmail.com>
2024-02-08 16:39:16 -0500

    use shadcn ui components in pages

--- site/src/app/login/page.tsx
+++ site/src/app/login/page.tsx
@@ -4,6 +4,9 @@
 // import { getProviders } from "next-auth/react";
 // import { redirect } from "next/navigation";
 import Jumbotron from "@/components/Jumbotron";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Page() {
   // const session = await getServerSession(authOptions); // maybe dont use server session
@@ -16,53 +19,30 @@ export default function Page() {
 
   // TODO: Implement login page
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
         <Jumbotron />
         <div>
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-2xl leading-none tracking-tight md:text-3xl lg:text-4xl">
             Welcome!
           </h1>
         </div>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <div className="mb-6">
-            <label
-              htmlFor="default-input"
-              className="mb-2 block text-sm font-bold text-gray-900 dark:text-white"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="default-input"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            />
+            <Label htmlFor="username">Username</Label>
+            <Input type="text" id="username" />
           </div>
         </div>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <div className="mb-6">
-            <label
-              htmlFor="default-input"
-              className="mb-2 block text-sm font-bold text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              type="text"
-              id="default-input"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            />
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password" />
           </div>
         </div>
       </div>
-
-      <div className="flex justify-center space-x-8">
-        <button className="rounded-md bg-blue-600 px-4 py-2 text-white">
-          Login
-        </button>
-        <button className="rounded-md bg-green-600 px-4 py-2 text-white">
-          Sign Up
-        </button>
+      <div className="flex justify-center space-x-3">
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
       </div>
     </section>
   );
