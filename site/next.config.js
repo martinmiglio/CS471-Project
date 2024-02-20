e1a1ddf04fff85af6784699f407b10ab12037b0b
Martin Miglio <marmig0404@gmail.com>
2024-02-19 22:25:15 -0500

    update routes for demo

--- site/next.config.js
+++ site/next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[{
-            protocol:"https",hostname:"source.unsplash.com"
-        },{
-            protocol:"https",hostname:"cdn.discordapp.com"
-        }]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
