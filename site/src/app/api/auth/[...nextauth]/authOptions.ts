a6642047344fa262fbbb9332361613a732b7858f
Martin Miglio <marmig0404@gmail.com>
2024-02-20 19:57:41 -0500

    add demo user

--- site/src/app/api/auth/[...nextauth]/authOptions.ts
+++ site/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -8,6 +8,8 @@ const schema = z.object({
   NEXTAUTH_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  TESTING_USER_ID: z.string().optional(),
+  TESTING_USER_EMAIL: z.string().optional(),
 });
 
 const env = schema.parse(process.env);
@@ -22,6 +24,24 @@ const authOptions: AuthOptions = {
   ],
   pages: {
     signIn: "/login",
+  },
+  callbacks: {
+    async session({ session }) {
+      if (
+        // process.env.VERCEL_ENV === "preview" ||
+        !env.TESTING_USER_ID ||
+        !env.TESTING_USER_EMAIL
+      ) {
+        return session;
+      }
+
+      session.user = {
+        name: "Testing",
+        email: env.TESTING_USER_EMAIL,
+      };
+
+      return session;
+    },
   },
   debug: process.env.NODE_ENV === "development",
   secret: env.NEXTAUTH_SECRET,
