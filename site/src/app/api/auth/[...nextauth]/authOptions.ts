35fb5dcb4d4d01d6c1b29098cd47002652f561c2
Martin Miglio <marmig0404@gmail.com>
2024-02-23 14:14:31 -0500

    Allow login to testing user on preview site (#30)

--- site/src/app/api/auth/[...nextauth]/authOptions.ts
+++ site/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { AuthOptions } from "next-auth";
+import type { AuthOptions, RequestInternal, User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { z } from "zod";
 
@@ -8,6 +9,8 @@ const schema = z.object({
   NEXTAUTH_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  TESTING_USER_ID: z.string().optional(),
+  TESTING_USER_EMAIL: z.string().optional(),
 });
 
 const env = schema.parse(process.env);
@@ -26,5 +29,33 @@ const authOptions: AuthOptions = {
   debug: process.env.NODE_ENV === "development",
   secret: env.NEXTAUTH_SECRET,
 };
+
+if (
+  process.env.VERCEL_ENV === "preview" &&
+  env.TESTING_USER_ID &&
+  env.TESTING_USER_EMAIL
+) {
+  authOptions.providers.push(
+    CredentialsProvider({
+      id: "biddr",
+      name: "Testing - Biddr.pro",
+      credentials: {},
+      authorize(
+        credentials: Record<string, string> | undefined,
+        req: Pick<RequestInternal, "headers" | "body" | "query" | "method">,
+      ): User {
+        return {
+          id: env.TESTING_USER_ID ?? "",
+          name: "Testing",
+          email: env.TESTING_USER_EMAIL,
+        };
+      },
+    }),
+  );
+  authOptions.session = {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  };
+}
 
 export default authOptions;
