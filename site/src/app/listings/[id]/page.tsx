4153e363828610059f7b0d2ee9bb302c99112b92
Martin Miglio <marmig0404@gmail.com>
2024-02-22 22:50:11 -0500

    add functionality to new bid button and bid history to page

--- site/src/app/listings/[id]/page.tsx
+++ site/src/app/listings/[id]/page.tsx
@@ -1,3 +1,4 @@
+import BidHistory from "@/components/BidHistory";
 import PlaceBidButton from "@/components/PlaceBidButton";
 import CountDown from "@/components/ui/CountDown";
 import { Button } from "@/components/ui/button";
@@ -23,39 +24,51 @@ export default async function ListingsPage({
     redirect("/404");
   }
 
+  const highestBid = !listing
+    ? { price: 0 }
+    : listing.bids.length == 0
+      ? listing
+      : listing.bids.reduce((a, b) => (a.price > b.price ? a : b));
+
   return (
-    <div className="flex w-full flex-col space-x-0 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-      <Image
-        src={listing.image}
-        alt={listing.title + " Image"}
-        className="mx-auto rounded-md object-cover"
-        width={400}
-        height={400}
-      />
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            {listing.title}
-            <div className="text-sm text-muted-foreground">
-              {listing.user.name}
+    <div className="flex flex-col space-y-2">
+      <div className="flex w-full flex-col space-x-0 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+        <Image
+          src={listing.image}
+          alt={listing.title + " Image"}
+          className="mx-auto rounded-md object-cover"
+          width={400}
+          height={400}
+        />
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              {listing.title}
+              <div className="text-sm text-muted-foreground">
+                {listing.user.name}
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            {listing.description}
+            <div className="mt-4 text-sm text-muted-foreground">
+              <div className="flex space-x-1">
+                <span>Time remaining: </span>
+                <CountDown endTime={new Date(listing?.expires)} />
+              </div>
+              <span>
+                ${highestBid.price.toFixed(2)} • {listing.bids.length} bid
+                {listing.bids.length === 1 ? "" : "s"}
+              </span>
             </div>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          {listing.description}
-          <div className="mt-4 text-sm text-muted-foreground">
-            <div className="flex space-x-1">
-              <span>Time remaining: </span>
-              <CountDown endTime={new Date(listing?.expires)} />
-            </div>
-            <span>${listing.price.toFixed(2)} • 0 bids</span>
-          </div>
-        </CardContent>
-        <CardFooter className="flex w-full justify-center space-x-2">
-          <PlaceBidButton />
-          <Button variant="secondary">Add to Watchlist</Button>
-        </CardFooter>
-      </Card>
+          </CardContent>
+          <CardFooter className="flex w-full justify-center space-x-2">
+            <PlaceBidButton listing={listing} />
+            <Button variant="secondary">Add to Watchlist</Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <BidHistory listing={listing} />
     </div>
   );
 }
