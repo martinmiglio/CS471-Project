31e2c68bae26a1ba1ff9d4ca1473856b8bbbb013
Martin Miglio <marmig0404@gmail.com>
2024-02-22 20:27:33 -0500

    Add scripts for demonstration (#31)

--- site/src/app/api/delete/route.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/lib/prisma/client";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  // delete all listings:
-  return NextResponse.json({
-    res: await prisma.listing.deleteMany(),
-  });
-}
