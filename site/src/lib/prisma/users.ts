d94402b9ed0ad586e00f68a7e9dc4b4dba40952c
Martin Miglio <marmig0404@gmail.com>
2024-03-04 16:05:03 -0500

    add backend for watchlist

---
+++ site/src/lib/prisma/users.ts
@@ -0,0 +1,40 @@
+import prisma from "./client";
+
+export const getWatchList = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: { email },
+    include: {
+      watchedListings: {
+        include: {
+          user: {
+            select: { name: true, image: true },
+          },
+          images: true,
+        },
+      },
+    },
+  });
+  return user?.watchedListings ?? [];
+};
+
+export const addToWatchList = async (email: string, listingId: string) => {
+  return await prisma.user.update({
+    where: { email },
+    data: {
+      watchedListings: {
+        connect: { id: listingId },
+      },
+    },
+  });
+};
+
+export const removeFromWatchList = async (email: string, listingId: string) => {
+  return await prisma.user.update({
+    where: { email },
+    data: {
+      watchedListings: {
+        disconnect: { id: listingId },
+      },
+    },
+  });
+};
