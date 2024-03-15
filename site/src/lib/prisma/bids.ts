27e6db4538c6aa32e4f9b6ede11f31eabd69dc25
Martin Miglio <marmig0404@gmail.com>
2024-03-14 22:21:22 -0400

    move bid validation logic to backend from route

--- site/src/lib/prisma/bids.ts
+++ site/src/lib/prisma/bids.ts
@@ -5,6 +5,39 @@ export async function placeNewBid(
   userEmail: string,
   price: number,
 ) {
+  const listing = await prisma.listing.findUnique({
+    where: { id: listingId },
+    select: {
+      expires: true,
+      price: true,
+      user: { select: { email: true } },
+      bids: {
+        select: { user: { select: { email: true } } },
+        orderBy: { createdAt: "desc" },
+      },
+    },
+  });
+
+  if (!listing) {
+    throw new Error("Listing not found");
+  }
+
+  if (listing.user.email === userEmail) {
+    throw new Error("You cannot bid on your own listing");
+  }
+
+  if (listing.bids.length > 0 && listing.bids[0].user.email === userEmail) {
+    throw new Error("You cannot bid on the same listing twice in a row");
+  }
+
+  if (new Date(listing.expires).getTime() < new Date().getTime()) {
+    throw new Error("Listing has expired");
+  }
+
+  if (price < listing.price) {
+    throw new Error("Bid must be greater than the current price");
+  }
+
   const res = await prisma.bid.create({
     data: {
       price,
